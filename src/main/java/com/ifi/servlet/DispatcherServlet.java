7c9602294d011dbec1dcb7b513212497eb1a2b4e
Legrand Eliott <legrande@b11p13.fil.univ-lille1.fr>
2019-11-04 09:06:19 +0100

    update tp2: part 5 with squeleton of exercices

---
+++ src/main/java/com/ifi/servlet/DispatcherServlet.java
@@ -0,0 +1,50 @@
+package com.ifi.servlet;
+
+import com.ifi.controller.HelloController;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(urlPatterns = "/*", loadOnStartup = 1)
+public class DispatcherServlet extends HttpServlet {
+
+    private Map<String, Method> uriMappings = new HashMap<>();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        System.out.println("Getting request for " + req.getRequestURI());
+        // TODO
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        // on enregistre notre controller au démarrage de la servlet
+        this.registerController(HelloController.class);
+    }
+
+    protected void registerController(Class controllerClass){
+        System.out.println("Analysing class " + controllerClass.getName());
+        // TODO
+    }
+
+    protected void registerMethod(Method method) {
+        System.out.println("Registering method " + method.getName());
+        // TODO
+    }
+
+    protected Map<String, Method> getMappings(){
+        return this.uriMappings;
+    }
+
+    protected Method getMappingForUri(String uri){
+        return this.uriMappings.get(uri);
+    }
+}
